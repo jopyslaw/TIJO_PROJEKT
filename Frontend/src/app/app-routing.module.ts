909a90596d98a925047d3b1a1ad000ad73204fea
jopyslaw <jopyslaw@gmail.com>
2023-11-16 18:33:32 +0100

    Added event details component

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { SchedulerComponent } from './components/scheduler/scheduler.component';
-
+import { EventDetailsComponent } from './components/event-details/event-details.component';
 
 const routes: Routes = [
   { path: '', component: AuthComponent },
   {
     path: 'scheduler',
     component: SchedulerComponent,
+  },
+  {
+    path: 'event/:eventId',
+    component: EventDetailsComponent,
   },
 ];
 
