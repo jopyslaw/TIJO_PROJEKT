f868b3b7b5327277fb65a1a8e36bed3bc7772c90
jopyslaw <jopyslaw@gmail.com>
2023-12-07 19:46:54 +0100

    Guard tests

--- Frontend/src/app/guards/token/token.guard.spec.ts
+++ Frontend/src/app/guards/token/token.guard.spec.ts
@@ -1,19 +1,40 @@
-import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { TokenGuard } from './token.guard';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TokenService } from 'src/app/services/token/token.service';
 
-import {TokenGuard} from "./token.guard";
-
-/*
 describe('tokenGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-      TestBed.runInInjectionContext(() => TokenGuard(...guardParameters));
+  let injector: TestBed;
+  let tokenService: TokenService;
+  let guard: TokenGuard;
+  let routeMock: any = { snapshot: {} };
+  let routeStateMock: any = { snapshot: {}, url: '/cookies' };
+  let routerMock = { navigate: jasmine.createSpy('navigate') };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [TokenGuard, { provide: Router, useValue: routerMock }],
+      imports: [HttpClientTestingModule],
+    });
+    injector = getTestBed();
+    tokenService = injector.inject(TokenService);
+    guard = injector.inject(TokenGuard);
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(guard).toBeTruthy();
+  });
+
+  it('should redirect an unauthenticated user to the main route', () => {
+    expect(guard.canActivate(routeMock, routeStateMock)).toEqual(false);
+    expect(routerMock.navigate).toHaveBeenCalledWith(['']);
+  });
+
+  it('should allow an authenticated user to access app', () => {
+    spyOn(tokenService, 'getToken').and.returnValue(
+      'fgjkhfdskjhgw4u38i7y45328ghjsdf67'
+    );
+    expect(guard.canActivate(routeMock, routeStateMock)).toEqual(true);
   });
 });
-*/
